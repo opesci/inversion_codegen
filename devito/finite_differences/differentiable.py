a18e506fa858e7280231c1ca2ef25a6d311733b0
Fabio Luporini <f.luporini12@imperial.ac.uk>
2021-04-14 18:25:57 +0200

    compiler: Drop unnecessary __new__ overrides

--- devito/finite_differences/differentiable.py
+++ devito/finite_differences/differentiable.py
@@ -349,12 +349,10 @@ class DifferentiableOp(Differentiable):
 
 class Add(DifferentiableOp, sympy.Add):
     __sympy_class__ = sympy.Add
-    __new__ = DifferentiableOp.__new__
 
 
 class Mul(DifferentiableOp, sympy.Mul):
     __sympy_class__ = sympy.Mul
-    __new__ = DifferentiableOp.__new__
 
     @property
     def _gather_for_diff(self):
@@ -397,17 +395,14 @@ class Mul(DifferentiableOp, sympy.Mul):
 class Pow(DifferentiableOp, sympy.Pow):
     _fd_priority = 0
     __sympy_class__ = sympy.Pow
-    __new__ = DifferentiableOp.__new__
 
 
 class Mod(DifferentiableOp, sympy.Mod):
     __sympy_class__ = sympy.Mod
-    __new__ = DifferentiableOp.__new__
 
 
 class EvalDiffDerivative(DifferentiableOp, EvalDerivative):
     __sympy_class__ = EvalDerivative
-    __new__ = DifferentiableOp.__new__
 
 
 class diffify(object):
