0c7d96201abebf515f20f636c79903f147e1862a
Fabio Luporini <f.luporini12@imperial.ac.uk>
2021-04-23 15:09:11 +0200

    more (decent shape finally...)

--- tests/test_dse.py
+++ tests/test_dse.py
@@ -1248,7 +1248,7 @@ class TestAliases(object):
         exprs = FindNodes(Expression).visit(op)
         sqrt_exprs = exprs[:2]
         assert all(e.write in arrays for e in sqrt_exprs)
-        assert all(e.expr.rhs.args[0].is_Pow for e in sqrt_exprs)
+        assert all(e.expr.rhs.is_Pow for e in sqrt_exprs)
         assert all(e.write._mem_heap and not e.write._mem_external for e in sqrt_exprs)
 
         tmp_exprs = exprs[4:6]
